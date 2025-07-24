c9ee4632d1e89f93bd5342e8967a213d21bd21af
Keshav <keshavm9978@gmail.com>
2025-07-24 09:58:23 +0530

    Created Basic Route

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-﻿
+﻿const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/health", (req, res) => {
+    console.log("Working")
+    res.send("Health Checked")
+})
+
+module.exports = app;
